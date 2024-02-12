ee9b845650ecee023fec95517134161590bca4d3
Gabriel Dominguez <gabguezdev@gmail.com>
2024-02-12 13:47:23 -0300

    handle bg video component lifecycle

--- src/components/background-video/background-video.tsx
+++ src/components/background-video/background-video.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 import BgVideoStyles from "./background-video.module.css";
 
@@ -8,8 +8,20 @@ export const BackgroundVideo = ({
   className = BgVideoStyles.bgVideo,
   src,
 }: Props) => {
+  const videoRef = useRef<HTMLVideoElement | null>(null);
+
+  useEffect(() => {
+    videoRef.current?.play() as unknown as React.MutableRefObject<HTMLVideoElement>;
+  }, []);
+
   return (
-    <video autoPlay loop muted className={className}>
+    <video
+      autoPlay={true}
+      loop
+      muted={true}
+      ref={videoRef}
+      className={className}
+    >
       <source src={src} type="video/mp4" />
       Your browser does not support the video tag.
     </video>
